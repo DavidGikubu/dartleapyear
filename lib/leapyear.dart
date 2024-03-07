4cf2829a78c50bae8ebf960c14da8b97c4d53780
Gikubu <davidndungu8203@gmail.com>
2024-03-07 20:18:08 +0300

    this creates an if statement loop to find the leap year.

--- lib/leapyear.dart
+++ lib/leapyear.dart
@@ -1,1 +1,19 @@
-void main() {}
+import 'dart:io';
+
+void main() {
+  print("welcome to leap year calculator!");
+  print("enter the year: ");
+  int? year = int.parse(stdin.readLineSync()!);
+}
+
+void isLeap(var year) {
+  if (year % 4 == 0) {
+    if (year % 100) {
+      print("$year is not a leap year..");
+    } else if (year % 4 == 0 && year % 400 == 0) {
+      print("$year is a leap year");
+    } else {
+      print("wrong input");
+    }
+  }
+}
